90c453dd6f1de3f38502257f95fb001ab7cf0434
hamuyuuki <13702378+hamuyuuki@users.noreply.github.com>
2021-07-01 11:02:53 +0900

    Render not localized template

--- lib/localized_controllers.rb
+++ lib/localized_controllers.rb
@@ -4,6 +4,7 @@ require "active_support/lazy_load_hooks"
 
 ActiveSupport.on_load(:before_initialize) do
   ActionDispatch::Request.prepend(LocalizedControllers)
+  ActionController::Base.prepend(LocalizedControllers)
 end
 
 module LocalizedControllers
@@ -14,6 +15,14 @@ module LocalizedControllers
       controller_class_for("#{params[:controller]}_#{path_parameters[:locale]}")
     else
       original_controller_class
+    end
+  end
+
+  def default_render(*args)
+    if params["locale"]
+      render(template: "#{params[:controller]}/#{params[:action]}")
+    else
+      super
     end
   end
 end
