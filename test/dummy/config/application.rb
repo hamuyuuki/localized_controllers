f80375fdacc6431c1c5a8289ce6a489c69409e14
hamuyuuki <13702378+hamuyuuki@users.noreply.github.com>
2021-07-02 10:31:12 +0900

    Test the localized_controllers function to use localized views

--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -14,6 +14,9 @@ module Dummy
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    # Permitted locales available for the application
+    I18n.available_locales = [:en, :ja]
   end
 end
 
