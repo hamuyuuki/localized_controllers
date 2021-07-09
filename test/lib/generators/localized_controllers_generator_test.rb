15e2f4a3ee3c9c0e99c661741b1a60648a7f2c25
hamuyuuki <13702378+hamuyuuki@users.noreply.github.com>
2021-07-10 07:20:03 +0900

    Add the test case of specifying the action for the localized_controllers generator

--- test/lib/generators/localized_controllers_generator_test.rb
+++ test/lib/generators/localized_controllers_generator_test.rb
@@ -8,9 +8,15 @@ class LocalizedControllersGeneratorTest < Rails::Generators::TestCase
   destination Rails.root.join("tmp/generators")
   setup :prepare_destination
 
-  test "should create only the localized controller" do
+  test "should generate only the localized controller when the action is not specified" do
     run_generator %w[LocalizableResources en]
     assert_file "app/controllers/localizable_resources_en_controller.rb"
     assert_no_file "app/views/localizable_resources/index.en.html.erb"
+  end
+
+  test "should generate the localized controller and its view when the action is specified" do
+    run_generator %w[LocalizableResources en index]
+    assert_file "app/controllers/localizable_resources_en_controller.rb"
+    assert_file "app/views/localizable_resources/index.en.html.erb"
   end
 end
