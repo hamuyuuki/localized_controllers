fe89396a4ccd59e6e433aa25fc55e210b33cae59
hamuyuuki <13702378+hamuyuuki@users.noreply.github.com>
2021-07-12 06:38:24 +0900

    Fix the test case of specifying the controller name with the module for the localized_controllers generator

--- lib/generators/localized_controllers/localized_controllers_generator.rb
+++ lib/generators/localized_controllers/localized_controllers_generator.rb
@@ -8,14 +8,23 @@ class LocalizedControllersGenerator < Rails::Generators::NamedBase
 
   def create_controller_file
     template "controller.rb",
-             "app/controllers/#{file_name}_#{locale}_controller.rb"
+             File.join(
+               "app/controllers",
+               class_path,
+               "#{file_name}_#{locale}_controller.rb"
+             )
   end
 
   def create_view_files
     actions.each do |action|
       @action = action
       template "view.html.erb",
-               "app/views/#{file_name}/#{action}.#{locale}.html.erb"
+               File.join(
+                 "app/views",
+                 class_path,
+                 file_name,
+                 "#{action}.#{locale}.html.erb"
+               )
     end
   end
 end
