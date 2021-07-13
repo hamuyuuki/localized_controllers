8508ac0df074435e52ef82cd9468f5bca3e67a21
hamuyuuki <13702378+hamuyuuki@users.noreply.github.com>
2021-07-14 06:20:58 +0900

    Use the another way to prepend with the anonymous module

--- lib/localized_controllers.rb
+++ lib/localized_controllers.rb
@@ -2,27 +2,31 @@
 
 require "active_support/lazy_load_hooks"
 
-ActiveSupport.on_load(:before_initialize) do
-  ActionDispatch::Request.prepend(LocalizedControllers)
-  ActionController::Base.prepend(LocalizedControllers)
-end
+ActiveSupport.on_load(:action_controller) do
+  ActionDispatch::Request.prepend(
+    Module.new do
+      def controller_class
+        original_controller_class = super
 
-module LocalizedControllers
-  def controller_class
-    original_controller_class = super
-
-    if path_parameters[:locale]
-      controller_class_for("#{params[:controller]}_#{path_parameters[:locale]}")
-    else
-      original_controller_class
+        if path_parameters[:locale]
+          controller_class_for(
+            "#{params[:controller]}_#{path_parameters[:locale]}"
+          )
+        else
+          original_controller_class
+        end
+      end
     end
-  end
-
-  def default_render(*args)
-    if params["locale"]
-      render(template: "#{params[:controller]}/#{params[:action]}")
-    else
-      super
+  )
+  ActionController::Base.prepend(
+    Module.new do
+      def default_render(*args)
+        if params["locale"]
+          render(template: "#{params[:controller]}/#{params[:action]}")
+        else
+          super
+        end
+      end
     end
-  end
+  )
 end
